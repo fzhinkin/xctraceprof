68579c3ee86ca1440a65e438c1221a51b7a9dab4
Filipp Zhinkin <filipp.zhinkin@gmail.com>
2023-09-03 20:18:36 +0200

    Fixed typo

--- profiler/build.gradle.kts
+++ profiler/build.gradle.kts
@@ -38,7 +38,7 @@ publishing {
 
             pom {
                 name.set("XCTrace JMH profilers")
-                description.set("Collection of JHM profiling using XCode Instruments as the underlying profiler.")
+                description.set("Collection of JHM profilers using XCode Instruments as the underlying profiler.")
                 url.set("https://github.com/fzhinkin/xctraceprof")
                 licenses {
                     license {
